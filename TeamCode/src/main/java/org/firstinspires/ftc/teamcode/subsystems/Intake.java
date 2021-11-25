c63071171ff707979d7c9a8ce4d766904da8cbaf
Nihar Kodkani <nihar.kodkani@gmail.com>
2021-11-24 22:24:19 -0500

    Intake class created
    Created intake class; initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+    DcMotorEx leftMotor;
+    DcMotorEx rightMotor;
+    HardwareMap hardwareMap;
+    public Intake(HardwareMap hw){
+        hardwareMap = hw;
+        leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+    }
+    public void stop(){
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+    }
+    public void setPower(double power){
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+    }
+}
